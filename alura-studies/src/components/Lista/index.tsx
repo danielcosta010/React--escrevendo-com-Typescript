320b81de801c0c4519f9af748127b903db8cc946
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-07-23 10:19:05 -0300

    Atualizando componente Lista/index.tsx

--- alura-studies/src/components/Lista/index.tsx
+++ alura-studies/src/components/Lista/index.tsx
@@ -1,36 +1,19 @@
-import React from 'react';
 import style from './Lista.module.scss'
+import Item from './Item';
+import { ITarefa } from '../../types/tarefa';
 
-function Lista() {
-  const tarefas = [
-    { tarefa: 'React',
-      tempo: '02:00:00'
-    },
-    { tarefa: 'Javascript',
-      tempo: '01:00:00'
-    }, 
-    {
-      tarefa: 'Typrscript',
-      tempo: '03:00:00'
-    }, 
-    {
-      tarefa: 'Typrscript',
-      tempo: '03:00:00'
-    }, 
-    {
-      tarefa: 'Typrscript',
-      tempo: '03:00:00'
-    }
-  ]
+
+function Lista({ tarefas }: { tarefas: ITarefa[]}) {
+  
   return (
     <aside  className={style.listaTarefas}>
       <h2> Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index} className={style.item}>
-            <h3>{item.tarefa}</h3>
-            <span>{item.tempo}</span>
-          </li>
+          <Item 
+          key={index}
+          {...item}
+        />
         ))}
       </ul>
     </aside>
