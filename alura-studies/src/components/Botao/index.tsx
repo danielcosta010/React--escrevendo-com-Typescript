902c97ac9f0acd29d22e993e597888a8fe731b6c
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-07-23 10:15:35 -0300

    Atualizando index.tsx

--- alura-studies/src/components/Botao/index.tsx
+++ alura-studies/src/components/Botao/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import style from './Botao.module.scss'
 
-class Botao extends React.Component {
+class Botao extends React.Component<{
+  children: React.ReactNode, 
+  type?: "button" | "submit" | "reset" | undefined
+}> {
   render() {
+    const { type = "button" } = this.props
     return (
-      <button className={style.botao}>
-        Botao
+      <button type={type} className={style.botao}>
+        {this.props.children}
       </button>
       );
       }
 }
+
+// export default (props: { children: React.ReactNode }) => (
+//   <button className={style.botao}>
+//     {props.children}
+//  </button>
+// )
 
 export default Botao
