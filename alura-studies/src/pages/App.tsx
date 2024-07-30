f6b6bec77eecfae6f63d382d086a5891cd150a1e
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-07-30 13:56:42 -0300

    Atualizando App.tsx

--- alura-studies/src/pages/App.tsx
+++ alura-studies/src/pages/App.tsx
@@ -1,18 +1,30 @@
 import React, { useState } from 'react';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
-import style from './App.module.scss'
 import Cronometro from '../components/Cronometro';
 import { ITarefa } from '../types/tarefa';
+import style from './App.module.scss'
 
 
 function App() {
-  const [tarefas, setTarefas] = useState<ITarefa[] | []>([])
+  const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })))
+  }
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas}/>
-      <Lista tarefas={tarefas}/>
-      <Cronometro />
+      <Lista 
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
+      <Cronometro selecionado={selecionado}/>
     </div>
   );
 }
